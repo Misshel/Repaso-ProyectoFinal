fc81bd938cc8ae256a9b43fa2b327db6cad5701c
Misshel <jenmish094@gmail.com>
2017-03-29 14:02:16 -0500

    login

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+var botonRegistrar= document.getElementById('BotonRegistrar');
+var botonInicio= document.getElementById('BotonInicio');
+
+botonRegistrar.addEventListener("click",function(){
+  window.location="index.html";
+});
+
+botonInicio.addEventListener("click",function(){
+  window.location="inicio.html";
+});
